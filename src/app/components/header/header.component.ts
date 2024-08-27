bf5d6149532c2ebe7fdd8db3cd9a40d97db5bffa
Irving Queval <irvingqueval@gmail.com>
2024-08-27 15:02:20 +0200

    search-bar added

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  @Output() search = new EventEmitter<string>();
 
+  onSearch(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.search.emit(value);
+  }
 }
